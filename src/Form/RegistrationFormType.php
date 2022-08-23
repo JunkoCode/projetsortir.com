12d25edb2cbd6917e00307a378eb8620412de3bf
Stevens <55973738+owneur@users.noreply.github.com>
2022-08-23 10:34:23 +0200

    Système d'Authentification + inscription

---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Utilisateur;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class RegistrationFormType extends ApplicationType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, $this->getConfiguration("Email", "Votre adresse email"))
+            ->add('plainPassword', PasswordType::class, [
+                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
+                'label' => 'Mot de passe',
+                'attr' =>
+                    ['autocomplete' => 'new-password',
+                    'placeholder'=>'Votre mot de passe'
+                    ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+            ])
+            ->add('pseudo', TextType::class, $this->getConfiguration("Pseudo", "Votre pseudo"))
+            ->add('nom', TextType::class, $this->getConfiguration("Nom", "Votre nom"))
+            ->add('prenom', TextType::class, $this->getConfiguration("Prénom", "Votre prénom"))
+            ->add('telephone', TextType::class, $this->getConfiguration("N° de téléphone", "Votre numéro de téléphone"))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Utilisateur::class,
+        ]);
+    }
+}
