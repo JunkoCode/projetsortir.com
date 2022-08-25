652e98b2cbc20340fc9c3082d429f3b9dc902448
JunkoCode <laposteaxel@gmail.com>
2022-08-25 09:16:48 +0200

    fonctionnalié Modifié profil: tests : -controleur OK -twig ok -modifiaction en BDD ok -redirection OK
    A faire :
    
    -Filtrage Admin sur le formulaire
    -CSS updateprofil.html.twig

--- src/Controller/UtilisateurController.php
+++ src/Controller/UtilisateurController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Utilisateur;
+use App\Form\UtilisateurType;
 use App\Repository\UtilisateurRepository;
 use Monolog\Handler\Curl\Util;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,13 +15,32 @@ class UtilisateurController extends AbstractController
 {
 
     #[Route('/profil/{id}', name: 'Profil')]
-    public function updateUser($id, UtilisateurRepository $utilisateurRepository): Response
+    public function showProfil($id, UtilisateurRepository $utilisateurRepository): Response
     {
 
         $utilisateur = $utilisateurRepository->find($id);
 
         return $this->render('utilisateur/profil.html.twig', [
             "utilisateur" => $utilisateur
+        ]);
+    }
+
+    #[Route('/updateprofil/{id}', name: 'UpdateProfil')]
+    public function updateProfil(Request $request, Utilisateur $utilisateur, UtilisateurRepository $utilisateurRepository): Response
+    {
+
+        $form = $this->createForm(UtilisateurType::class, $utilisateur);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $utilisateurRepository->add($utilisateur, true);
+
+            return $this->redirectToRoute('home', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('utilisateur/updateprofil.html.twig', [
+            'utilisateur' => $utilisateur,
+            'updateForm' => $form,
         ]);
     }
 
