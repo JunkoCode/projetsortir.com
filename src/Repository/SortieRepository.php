b7714b0e9100d4dc57f304e54d06df82bb591c88
JunkoCode <laposteaxel@gmail.com>
2022-08-31 19:27:48 +0200

    FIX bouton submit dans le twig et filtre de recherche part mot clé

--- src/Repository/SortieRepository.php
+++ src/Repository/SortieRepository.php
@@ -81,7 +81,7 @@ class SortieRepository extends ServiceEntityRepository
         if(!empty($data->filtreSortieMotCle)){
             $query
                 ->andWhere('s.nom LIKE :filtreSortieMotCle')
-                ->setParameter('filtreSortieMotCle', $data->filtreSortieMotCle);
+                ->setParameter('filtreSortieMotCle', '%'.$data->filtreSortieMotCle.'%');
         }
 
         if (!empty($data->filtreSortieDateMin)){
