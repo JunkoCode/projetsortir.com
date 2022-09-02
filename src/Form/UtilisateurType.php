3dbb618d5590d9db9bc77475f524d00cb41ce63d
Stevens <55973738+owneur@users.noreply.github.com>
2022-09-02 11:30:16 +0200

    Hashage mdp admin

--- src/Form/UtilisateurType.php
+++ src/Form/UtilisateurType.php
@@ -7,9 +7,12 @@ use App\Entity\Utilisateur;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class UtilisateurType extends AbstractType
 {
@@ -18,7 +21,26 @@ class UtilisateurType extends AbstractType
         $builder
             ->add('email')
             //->add('roles')
-            ->add('password')
+            ->add('password', PasswordType::class, [
+            // instead of being set onto the object directly,
+            // this is read and encoded in the controller
+            //'mapped' => false,
+            'label' => 'Mot de passe',
+            'attr' =>
+                ['autocomplete' => 'new-password'
+                ],
+            'constraints' => [
+                new NotBlank([
+                    'message' => 'Please enter a password',
+                ]),
+                new Length([
+                    'min' => 6,
+                    'minMessage' => 'Votre mot de passe doit être au minimum de {{ limit }} caractères',
+                    // max length allowed by Symfony for security reasons
+                    'max' => 4096,
+                    ]),
+                ],
+            ])
             ->add('pseudo')
             ->add('nom')
             ->add('prenom')
